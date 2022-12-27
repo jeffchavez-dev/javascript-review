8725b96824c5a15a9f97a205440a9bdeedf342f1
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-12-27 12:20:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -33,11 +33,49 @@ for (const station of stations) {
   list.appendChild(listItem);
 }
 
-const lessonEl = document.querySelector(".lesson")
-const checkboxes = lessonEl.querySelectorAll('input[type="checkbox"]')
 
-checkboxes.forEach(checkbox => {
-  if(checkbox.checked) {
-    
+const lessonOne = [
+  {greek : "ἀγάπη, ἡ", gloss: "love"},
+  {greek : "γῆ ἡ" , gloss: "earth, land, ground (geology)"},
+  {greek : "ζωή ἡ", gloss: "life (zoo, zoology)"},
+  {greek : "φωνή, ἡ ", gloss: "voice, sound (phonetics, phonograph, phone)"},
+  {greek : "ἀλήθεια, ἡ", gloss: "truth"},
+  {greek : "ἁμαρτία, ἡ", gloss:  "sin (hamartiology—the theological study of sin)"},
+  {greek : "βασιλεία, ἡ" , gloss: "kingdom, reign (basilica)"},
+  {greek : "δόξα, ἡ" , gloss: "glory, majesty (doxology)"},
+  {greek : "ἐκκλησία, ἡ" , gloss: "congregation, assembly, church (ecclesiastical)"},
+  {greek : "ἡμέρα, ἡ" , gloss: "day (ephemeral, “for a day”)"},
+  {greek : "καρδία, ἡ", gloss:  "heart (cardiologist)"},
+  {greek : "δέ" , gloss: "and, but, now"},
+  {greek : "καί" , gloss: "and, even, also"},
+  {greek : "μέν" , gloss: "on the one hand, indeed"},
+  {greek : "ὁ, ἡ, τό" , gloss: "the"}
+]
+const lessonTwo = [
+  {greek :"ἀδελφός, ὁ ", gloss: "brother (and sister) "},
+  {greek :"ἄνθρωπος, ὁ ", gloss: "man, human being, husband (anthropology)"},
+  {greek :"θεός, ὁ ", gloss: "God (theology)"},
+   {greek :"κόσμος, ὁ ", gloss: "world, universe; adornment (cosmos, cosmology, cosmetics)"},
+   {greek :"κύριος, ὁ ", gloss: "Lord, master, sir"},
+  {greek :"λόγος, ὁ ", gloss: "word, message, account (logic, theology,psychology)"},
+  {greek :"νόμος, ὁ ", gloss: "law, principle (theonomists)"},
+  {greek :"οὐρανός, ὁ", gloss: " heaven, sky (Uranus)"},
+  {greek :"υἱός, ὁ ", gloss: "son, descendant"},
+  {greek :"Χριστός, ὁ ", gloss: "Christ, Messiah, Anointed One (Christ)"},
+  {greek :"ἔργον, τό", gloss: "work, deed (ergonomic)"},
+  {greek :"εὐαγγέλιον, τό ", gloss: "good news, gospel (evangelistic, evangelical)"},
+  {greek :"ἱερόν, τό ", gloss: "temple, sanctuary"},
+   {greek :"σημεἱον, τό ", gloss: "sign (semiotics)"},
+  {greek :"τέκνον, τό", gloss: " child, son, descendant"}
+]
+const lessonEl = document.querySelector(".lesson")
+const isLessonChecked = lessonEl.querySelectorAll('input[type="checkbox"]')
+
+isLessonChecked.forEach(lessonChecked => { //loop through each of the checkboxes
+  if(lessonChecked.checked) {
+    const review = document.createElement("div")
+    review.innerHTML = `${lessonChecked.value}`;
+    review.classList.add('lesson-added');
+    toReview.appendChild(review);
   }
 })
