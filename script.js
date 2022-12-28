a414b376e1a68ce60a73127c27cb00afd4dd10a4
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-12-28 22:35:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -120,3 +120,22 @@ function troubleshooting() {
 }
 
 console.log(troubleshooting())
+
+/**
+ * The code below tells the browser to ask you for a number
+ * then if that number is `6`, it returns `true` otherwise it returns `false`
+ * 
+ * Change this code so it returns `true` when the number is greater than or equal to 10, and false if it is less than 10
+ */
+
+number = Number(prompt("enter a number"));
+
+function numberChecker() {
+	if(number <= 10 ) {
+		return true;10
+	} else {
+		return false;
+	}
+}
+
+console.log(numberChecker)
