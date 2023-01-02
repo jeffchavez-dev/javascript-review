b732b791c2e9adbcf4c5866510ac546cc49b586d
Jeff Chavez <jeff.chavez0828@gmail.com>
2023-01-02 21:38:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -97,77 +97,77 @@ let errored = false;
 
 console.log("\n---------------------------------\n\n")
 
-if(values.testGroup === "a") {
-	if(values.birthYear !== 1948) {
+if (values.testGroup === "a") {
+	if (values.birthYear !== 1948) {
 		console.error(`birthYear is incorrect, it's currently: "${values.birthYear}"`);
 		errored = true;
 	}
-	
-	if(values.thisYear !== 1965) {
+
+	if (values.thisYear !== 1965) {
 		console.error(`thisYear is incorrect, it's currently: "${values.thisYear}"`);
 		errored = true;
 	}
-	
-	if(values.firstName !== "Carlos") {
+
+	if (values.firstName !== "Carlos") {
 		console.error(`firstName is incorrect, it's currently: "${values.firstName}"`);
 		errored = true;
 	}
-	
-	if(values.lastName !== "Stevenson") {
+
+	if (values.lastName !== "Stevenson") {
 		console.error(`lastName is incorrect, it's currently: "${values.lastName}"`);
 		errored = true;
 	}
-	
-	if(values.greeting !== "Hello! My name is Carlos Stevenson and I am 17 years old.") {
+
+	if (values.greeting !== "Hello! My name is Carlos Stevenson and I am 17 years old.") {
 		console.error(`greeting is incorrect, it's currently: "${values.greeting}"`);
 		errored = true;
 	}
 }
 
-if(values.testGroup === "b") {
-	if(values.birthYear !== 1948) {
+if (values.testGroup === "b") {
+	if (values.birthYear !== 1948) {
 		console.error(`birthYear is incorrect, it's currently: "${values.birthYear}"`);
 		errored = true;
 	}
-	
-	if(values.thisYear !== 1965) {
+
+	if (values.thisYear !== 1965) {
 		console.error(`thisYear is incorrect, it's currently: "${values.thisYear}"`);
 		errored = true;
 	}
-	
-	if(values.firstName !== "Carlos") {
+
+	if (values.firstName !== "Carlos") {
 		console.error(`firstName is incorrect, it's currently: "${values.firstName}"`);
 		errored = true;
 	}
-	
-	if(values.lastName !== "Stevenson") {
+
+	if (values.lastName !== "Stevenson") {
 		console.error(`lastName is incorrect, it's currently: "${values.lastName}"`);
 		errored = true;
 	}
 
-	if(values.age !== 17) {
+	if (values.age !== 17) {
 		console.error(`age is incorrect, it's currently: "${values.age}"`);
 		errored = true;
 	}
 
-	if(values.fullName !== "Carlos Stevenson") {
+	if (values.fullName !== "Carlos Stevenson") {
 		console.error(`fullName is incorrect, it's currently: "${values.fullName}"`);
 		errored = true;
 	}
-	
-	if(values.greeting !== "Hello! My name is Carlos Stevenson and I am 17 years old.") {
+
+	if (values.greeting !== "Hello! My name is Carlos Stevenson and I am 17 years old.") {
 		console.error(`greeting is incorrect, it's currently: "${values.greeting}"`);
 		errored = true;
 	}
 }
 
-if(!errored && values.testGroup === "a") {
+if (!errored && values.testGroup === "a") {
 	console.log("Congrats! Move onto the next step!");
 } else if (errored && values.testGroup === "a") {
 	console.log("Try again")
 }
 
-if(!errored && values.testGroup === "b") {
+if (!errored && values.testGroup === "b") {
 	console.log("Congrats! Move onto the next lesson!");
 } else if (errored) {
 	console.log("Try again")
@@ -260,13 +260,13 @@ console.log(troubleshooting())
 //                   'SYB4f65hf75f736463;Stalybridge',
 //                   'HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield'];
 
-// for (const station of stations) {
-//   // write your code just below here
-//   const extractLetters = station.slice(0,3)
-//   const findSemiColon = station.indexOf(';');
-//   const extractName = station.slice(findSemiColon + 1)
-//   const result = `${extractLetters}: ${extractName}`;
-//   const listItem = document.createElement('li');
-//   listItem.textContent = result;
-//   list.appendChild(listItem);
-// }
+for (const station of stations) {
+	// write your code just below here
+	const extractLetters = station.slice(0, 3)
+	const findSemiColon = station.indexOf(';');
+	const extractName = station.slice(findSemiColon + 1)
+	const result = `${extractLetters}: ${extractName}`;
+	const listItem = document.createElement('li');
+	listItem.textContent = result;
+	list.appendChild(listItem);
+}
