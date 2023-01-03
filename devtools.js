223f62419b45607b3a5be960ab84bb4564161199
Jeff Chavez <jeff.chavez0828@gmail.com>
2023-01-03 09:16:17 +0800

    Update devtools.js

--- devtools.js
+++ devtools.js
@@ -40,3 +40,13 @@ books.forEach(book => {
 
 //counting
 console.count('p')
+
+//timing
+console.time('fetching data')
+
+fetch('https://api.github.com/users/wesbos')
+    .then(data => data.json())
+    .then(data => {
+        console.timeEnd('fetching data')
+        console.log(data)
+    })
