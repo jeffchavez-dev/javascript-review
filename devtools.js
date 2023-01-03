efacb3593b60670d89b52ca7e392a698c3b2fe68
Jeff Chavez <jeff.chavez0828@gmail.com>
2023-01-03 09:01:24 +0800

    Update devtools.js

---
+++ devtools.js
@@ -0,0 +1,10 @@
+const books = [{ name: 'Knowing Sin', author: 'Mark Jones'}, {name: 'Institutes', author: 'John Calvin'}]
+
+
+
+function makeGreen(){
+    const p = document.querySelector('p');
+    p.style.color = '#BADA55';
+    p.style.fontSize = '10em';
+}
+
