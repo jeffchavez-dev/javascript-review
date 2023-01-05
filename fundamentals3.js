fba24eb9c94bdc3354d5465520fd0c52b1dcbda9
Jeff Chavez <jeff.chavez0828@gmail.com>
2023-01-05 21:21:45 +0800

    Update fundamentals3.js

--- fundamentals3.js
+++ fundamentals3.js
@@ -10,3 +10,29 @@ function favoriteAnimal(animal) {
   
   hello('Ari'); // Hello Ari!
   hello();      // Hello Chris!
+
+  const originals = [1, 2, 3];
+
+const doubled = originals.map((item) => item * 2);
+
+console.log(doubled); // [2, 4, 6]
+console.clear()
+
+//Function scope and conflicts
+
+
+const x = 1;
+
+    function a() {
+      const y = 2;
+    }
+
+    function b() {
+      const z = 3;
+    }
+
+    function output(value) {
+      const para = document.createElement('p');
+      document.body.appendChild(para);
+      para.textContent = `Value: ${value}`;
+    }
