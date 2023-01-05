1029387c76c5a75f0340c2aa717e6696d4b528f3
Jeff Chavez <jeff.chavez0828@gmail.com>
2023-01-05 21:10:34 +0800

    Update fundamentals3.js

--- fundamentals3.js
+++ fundamentals3.js
@@ -5,6 +5,20 @@ function favoriteAnimal(animal) {
   console.log(favoriteAnimal('bird'))
 
 
+
+  const btn = document.querySelector('button');
+  const canvas = document.querySelector('canvas');
+  const ctx = canvas.getContext('2d');
+
+  let WIDTH = document.documentElement.clientWidth;
+  let HEIGHT = document.documentElement.clientHeight;
+
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+
+  function random(number) {
+    return Math.floor(Math.random()*number);
+  }
   function draw() {
     ctx.clearRect(0,0,WIDTH,HEIGHT);
     for (let i = 0; i < 100; i++) {
@@ -14,3 +28,7 @@ function favoriteAnimal(animal) {
       ctx.fill();
     }
   }
+
+
+  btn.addEventListener('click',draw);
+  draw();
