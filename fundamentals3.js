4a336216bfbe84ac40a35e60da9e59646a05a692
Jeff Chavez <jeff.chavez0828@gmail.com>
2023-02-06 20:41:10 +0800

    Update fundamentals3.js

--- fundamentals3.js
+++ fundamentals3.js
@@ -76,3 +76,20 @@
 //   }
  
 //   console.log(favoriteAnimal('Knowing Christ'))
+
+
+function checkAge(age) {
+    if (age >= 18) {
+      return true;
+    } else {
+      return confirm('Do you have permission from your parents?');
+    }
+  }
+  
+  let age = prompt('How old are you?', 18);
+  
+  if ( checkAge(age) ) {
+    alert( 'Access granted' );
+  } else {
+    alert( 'Access denied' );
+  }
